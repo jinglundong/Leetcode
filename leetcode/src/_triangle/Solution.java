995a0982cb4a61af4758cd5153dd87262f93a5d2
Jinglun <jinglundong@gmail.com>
2012-12-07 00:36:59 -0500

    add question description of question triangle

--- leetcode/src/_triangle/Solution.java
+++ leetcode/src/_triangle/Solution.java
@@ -2,19 +2,35 @@ package _triangle;
 
 import java.util.ArrayList;
 
+/**
+ * Given a triangle, find the minimum path sum from top to bottom. Each step you
+ * may move to adjacent numbers on the row below.
+ * 
+ * For example, given the following triangle [ [2], [3,4], [6,5,7], [4,1,8,3] ]
+ * The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+ * 
+ * Note: Bonus point if you are able to do this using only O(n) extra space,
+ * where n is the total number of rows in the triangle.
+ * 
+ * @author Jinglun
+ * 
+ */
 public class Solution {
-    public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        if (triangle == null){
-            return 0;
-        }
-        ArrayList<Integer> tmp = new ArrayList<Integer>(triangle.get(triangle.size()-1));
-        for (int i=triangle.size()-2; i>=0; i--){
-            for (int j=0; j<i+1; j++){
-                tmp.set(j, triangle.get(i).get(j) + Math.min(tmp.get(j), tmp.get(j+1)));
-            }
-        }
-        return tmp.get(0);
-    }
+	public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		if (triangle == null) {
+			return 0;
+		}
+		ArrayList<Integer> tmp = new ArrayList<Integer>(triangle.get(triangle
+				.size() - 1));
+		for (int i = triangle.size() - 2; i >= 0; i--) {
+			for (int j = 0; j < i + 1; j++) {
+				tmp.set(j,
+						triangle.get(i).get(j)
+								+ Math.min(tmp.get(j), tmp.get(j + 1)));
+			}
+		}
+		return tmp.get(0);
+	}
 }
